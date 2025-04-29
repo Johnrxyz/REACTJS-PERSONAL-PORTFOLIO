47af57d052088f0491040cbf631cfc043384dbae
Johnrxyz <johnreymansilungan122@gmail.com.com>
2025-04-29 23:10:39 +0800

    remove alt?

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe(props) {
             <p className="aboutDes">A PHOTO OF ME:</p>
 
             <div className="imgHolder">
-                <img src={props.myImage} alt="a picture of me" style={{width:'100%', height:'auto'}}/>
+                <img src={props.myImage}  style={{width:'100%', height:'auto'}}/>
             </div>
         </>
     )
